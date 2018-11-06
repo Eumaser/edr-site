1300b94f51ae4bc921f273982b4859427520fcf8
Eumer Ramos <eumerjoseph.ramos@yahoo.com>
2018-11-06 13:32:58 +0800

    added search function

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,10 +1,23 @@
 class Anime < ApplicationRecord
   validates  :name, presence: true
 
+  def self.inquire(params)
+    animes = all.order(:name) # for not existing params args
+  #  animes = animes.where("name like ?", "%#{params[:ids]}%") if params[:ids]
+  #
+    animes = animes.where(id: params[:ids]) if params[:ids].present?
+    animes = animes.where(location: params[:location]) if params[:location].present?
+    animes
+  end
+
+
+
   LOCATIONS = [
     'Kanon',
     'Hoshino Ai',
     'Ionassal',
   ]
+
+
 
 end
